dff8683b663d37c1ac70cc8786cd2a88d4667d8e
querist <querist@questor.local>
2019-11-10 13:21:13 -0600

    minor change to main.cpp to use tft

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ DigitalOut led2(LED2);
 Adafruit_TFTLCD_16bit_STM32 tft(LED2, 240, 320);
 int main()
 {
+	tft.begin();
+	tft.fillScreen(RED);
+
     while(1) {
         led1 = !led1;
         ThisThread::sleep_for(1000);
